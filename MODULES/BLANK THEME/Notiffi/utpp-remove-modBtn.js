0502b1630d203e356715ca977dd8cdd7ec61e818
foehnn. <87560864+thewanderingwhispers@users.noreply.github.com>
2026-06-26 17:58:54 +0200

    Create utpp-remove-modBtn.js

---
+++ MODULES/BLANK THEME/Notiffi/utpp-remove-modBtn.js
@@ -0,0 +1,20 @@
+$(function () {
+
+  const update = () => {
+    const notif = $('#notiffi_button').is(':visible');
+
+    if (notif) {
+      $('.sidebar').addClass('has-notiffi');
+    } else {
+      $('.sidebar').removeClass('has-notiffi');
+    }
+  };
+
+  update();
+
+  new MutationObserver(update).observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+
+});
