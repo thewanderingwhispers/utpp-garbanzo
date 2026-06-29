f412a7b0f2abb15ce93e57520600a36c6a15a633
foehnn. <87560864+thewanderingwhispers@users.noreply.github.com>
2026-06-29 14:42:53 +0200

    Update utpp-remove-modBtn.js

--- MODULES/BLANK THEME/Notiffi/utpp-remove-modBtn.js
+++ MODULES/BLANK THEME/Notiffi/utpp-remove-modBtn.js
@@ -1,4 +1,4 @@
-/* $(function () {
+$(function () {
 
   const update = () => {
     const notif = $('#notiffi_button').is(':visible');
@@ -15,37 +15,6 @@
   new MutationObserver(update).observe(document.body, {
     childList: true,
     subtree: true
-  });
-
-}); */
-
-$(function () {
-
-  const update = () => {
-    const $sidebar = $('.sidebar');
-
-    const notif = $('#notiffi_button').is(':visible');
-    const logbook = $('#logbook-toggle').is(':visible');
-    const frqcy = $('#frqcy-toggle').is(':visible');
-    const rpgPanel = $('#rpg-panel-button').is(':visible');
-    const pins = $('#fa-pins-button').is(':visible');
-    const recentTopics = $('#KRSN-button').is(':visible');
-
-    $sidebar.toggleClass('has-notiffi', notif);
-    $sidebar.toggleClass('has-logbook', logbook);
-    $sidebar.toggleClass('has-frqcy', frqcy);
-    $sidebar.toggleClass('has-rpg-panel', rpgPanel);
-    $sidebar.toggleClass('has-pins', pins);
-    $sidebar.toggleClass('has-recent-topics', recentTopics);
-  };
-
-  update();
-
-  new MutationObserver(update).observe(document.body, {
-    childList: true,
-    subtree: true,
-    attributes: true,
-    attributeFilter: ['style', 'class']
   });
 
 });
