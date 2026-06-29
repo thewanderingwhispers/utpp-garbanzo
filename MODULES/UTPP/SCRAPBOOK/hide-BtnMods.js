765995b17d2ba6b6b25c1a370effbee466f531e2
foehnn. <87560864+thewanderingwhispers@users.noreply.github.com>
2026-06-29 16:16:39 +0200

    Update and rename hide-scrapbook.js to hide-BtnMods.js

--- MODULES/UTPP/SCRAPBOOK/hide-BtnMods.js
+++ MODULES/UTPP/SCRAPBOOK/hide-BtnMods.js
@@ -1,6 +1,7 @@
 $(function () {
 
   if (typeof _userdata !== 'undefined' && _userdata.session_logged_in !== 1) {
+    $('#notiffi_button').hide();
     $('#fa-pins-button').hide();
     $('#FAM-button-open').hide();
     $('#logbook-toggle').hide();
