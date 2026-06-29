ec6ac99fd1f7f190073956e97eba89caa86f3f2a
foehnn. <87560864+thewanderingwhispers@users.noreply.github.com>
2026-06-29 15:32:43 +0200

    Update hide-scrapbook.js

--- MODULES/UTPP/SCRAPBOOK/hide-scrapbook.js
+++ MODULES/UTPP/SCRAPBOOK/hide-scrapbook.js
@@ -3,6 +3,10 @@ $(function () {
   if (typeof _userdata !== 'undefined' && _userdata.session_logged_in !== 1) {
     $('#fa-pins-button').hide();
     $('#FAM-button-open').hide();
+    $('#logbook-toggle').hide();
+    $('#frqcy-toggle').hide();
+    $('##rpg-panel-button').hide();
+    $('##KRSN-button').hide();
   }
 
 });
